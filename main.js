e9957e7b643de70fb1753e0f66968786f51e8c15
Helen Root <helenroot1@gmail.com>
2022-12-21 13:13:08 +0000

    Human: fix use of API

--- main.js
+++ main.js
@@ -131,11 +131,13 @@ addInteractivity()
 
 async function isWordValid(word) {
   // Make a GET request to the Datamuse API to check if the word is valid
-  const response = await fetch(`https://api.datamuse.com/words?sp=${word}`);
+  // limit to returning 1 word because just want to check if given word is valid
+  // and include the frequency score to be used later for scoring
+  const response = await fetch(`https://api.datamuse.com/words?sp=${word}&max=1&md=f`);
 
   // Convert the response to JSON
   const data = await response.json();
 
   // If the array has at least one item and the first item's word property matches the word passed to the function, the word is valid
-  return Array.isArray(data) && data.length > 0 && data[0].word === word;
+  return Array.isArray(data) && data.length > 0 && data[0].word.toLowerCase() === word.toLowerCase();
 }
